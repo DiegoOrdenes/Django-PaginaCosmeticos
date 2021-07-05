ac6265e6053b43afddc900155b56cbcff5dd6faa
fabi057 <81185639+fabi057@users.noreply.github.com>
2021-07-05 13:01:49 -0500

    Add files via upload

---
+++ CosmeticoVenta/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+#Modelo del producto
+class Producto(models.Model):
+   idProducto = models.AutoField(primary_key=True,verbose_name='Id de producto')
+   nombreProducto = models.CharField(max_length=50,verbose_name='Nombre del producto')
+   descripcion = models.CharField(max_length=50,verbose_name='Descripcion')
+   precio = models.IntegerField(verbose_name='Precio producto')
+
+
+   def __str__(self):
+       return self.nombreProducto
+
+
+#Modelo del cliente
+class Cliente(models.Model):
+   runCliente = models.CharField(max_length=9,primary_key=True, verbose_name='Run')
+   nombreCompleto = models.CharField(max_length=20, verbose_name='NombreCompleto')
+   telefono = models.IntegerField(verbose_name='Telefono')
+   gmail = models.CharField(max_length=50,verbose_name='Correo electronico')
+ 
+   def __str__(self):
+       return self.runCliente
+
+#Modelo de las ventas realizadas
+class Ventas(models.Model):
+    idVenta = models.AutoField(primary_key=True,verbose_name='Venta')
+    total = models.IntegerField(verbose_name='Total')
+
+
+    def __str__(self):
+       return self.idVenta
---
+++ CosmeticoVenta/views.py
@@ -0,0 +1,149 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import Producto
+from .models import Cliente
+from .forms import ProductoForm,ClienteForm
+# Create your views here.
+
+#aqui van todas las funciones  de nuestra pagina
+def home(request):
+    return render(request,'CosmeticoVenta/index.html')
+
+def nosotros(request):
+    return render(request,'CosmeticoVenta/nosotros.html')
+
+def blog(request):
+    return render(request,'CosmeticoVenta/blog.html')
+ 
+def contact(request):
+    return render(request,'CosmeticoVenta/contact.html')
+
+def contacto(request):
+    return render(request,'CosmeticoVenta/contacto.html')
+
+def conversor_moneda(request):
+    return render(request,'CosmeticoVenta/conversor_moneda.html')
+
+def enviado(request):
+    return render(request,'CosmeticoVenta/enviado.html')
+
+def shop(request):
+    return render(request,'CosmeticoVenta/shop.html')
+
+def register(request):
+    return render(request,'CosmeticoVenta/registrarvista.html')
+
+def Clientes(request):
+    return render(request,'CosmeticoVenta/Clientes.html')
+
+def ajustes(request):
+    return render(request,'CosmeticoVenta/ajustes.html')
+
+def mostrar_api(request):
+    return render(request,'CosmeticoVenta/mostrar_api.html')
+
+
+
+#mostraar los datos en una tabla
+def shop(request):
+    listaProductos = Producto.objects.all()
+    datos = {
+        'productos':listaProductos,
+    }
+    return render(request,'CosmeticoVenta/shop.html',datos)
+
+
+#funcion para ir  a la vista de agregar producto
+def form_producto(request):
+    datos = {
+        'form':ProductoForm()
+    }
+
+    if(request.method == 'POST'):
+        formulario = ProductoForm(request.POST)
+        if formulario.is_valid():
+            formulario.save() #Realiza un insert en BD
+            datos['mensaje'] = 'Guardado Correctamente'
+            
+    return render(request,'CosmeticoVenta/form_producto.html',datos)
+
+
+ #------------------------------------------------------------------------
+
+#eliminaar el producto
+def form_del_producto(request,id):
+    producto = Producto.objects.get(idProducto=id)
+    producto.delete()
+
+    return redirect(to='shop')
+
+
+
+
+
+
+
+
+
+
+
+
+
+#-----------------------------------------------------------------------------
+
+#mostrar  los clientes en tabla
+def Clientes(request):
+    listaClientes = Cliente.objects.all()
+    datos = {
+        'cliente':listaClientes,
+    }
+    return render(request,'CosmeticoVenta/Clientes.html',datos)
+
+
+
+
+#funcion para ir  a la vista de agregar clientes
+def form_clientes(request):
+    datos = {
+        'form_cli':ClienteForm()
+    }
+    if(request.method == 'POST'):
+        formulario = ClienteForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            datos['mensaje'] = 'Guardado Correctamente'
+            return redirect(to='home')
+    return render(request,'CosmeticoVenta/form_clientes.html',datos)
+
+
+#-------------------------------------------
+#Modificar los clientes(Admin)
+def form_mod_clientes(request, id):
+
+    cliente = get_object_or_404(Cliente, runCliente=id)
+
+    data = {
+        'form': ClienteForm(instance=cliente)
+    }
+    
+    if request.method == 'POST':
+        formulario = ClienteForm(data=request.POST, instance=cliente, files=request.Files)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect(to="Clientes")
+        data["form"] = formulario
+
+
+
+    return render(request,'CosmeticoVenta/form_mod_clientes.html')
+
+
+#Eliminar un Cliente registrado
+def form_del_clientes(request, id):
+    cliente = Cliente.objects.get(runCliente=id)
+    cliente.delete()
+
+    return redirect(to='Clientes')
+
+
+
+
